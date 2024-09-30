6b82415af00e628c0f3ab30151f82bdf28ce3527
Dennis Ramirez <dr04860@gmail.com>
2024-09-30 15:00:57 -0600

    Remove large video files

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({
+    super.key,
+    required this.videos
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      itemCount: videos.length,
+      itemBuilder: (context, index){
+        final videoPost = videos[index];Container( color: Colors.red);
+
+        return Stack(
+          children: [
+            
+
+            //Bottones
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
